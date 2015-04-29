c73abf79e5a94e068bd70f6e48e3e3adc5946f8c
Marius Gundersen <Marius.Gundersen@ciber.com>
2015-04-29 21:06:13 +0200

    improved help

--- help.js
+++ help.js
@@ -1,10 +1,10 @@
 const co = require('co');
 
 module.exports = co.wrap(function*(name, user){
-  return 'Hei på deg, har du lyst på is?\n'+
-    '```\n'+[
-    'isbot status                - se liste over is',
-    'isbot jeg er <ansattnummer> - registrer deg så du kan kjøpe is',
-    'isbot kjøp <navn på is>     - kjøp en is'
-  ].join('\n')+'\n```';
+  return [
+    'Hei på deg, har du lyst på is?',
+    '`isbot status` se liste over is',
+    '`isbot jeg er <ansattnummer>` registrer deg så du kan kjøpe is',
+    '`isbot kjøp <navn på is>` kjøp en is'
+  ].join('\n');
 });
