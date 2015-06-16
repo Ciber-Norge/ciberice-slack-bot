45c18562ad70627ec1fb5f2fa52d4db5e38e4d3b
Marius Gundersen <gundersen@gmail.com>
2015-06-16 09:32:10 +0200

    removed try-catch

--- myBadges.js
+++ myBadges.js
@@ -1,17 +1,13 @@
 const co = require('co');
 const api = require('./api');
 
-module.exports = co.wrap(function* getStatus(user, parameter){
-  try {
-    const result = yield api.myBadges(user);
-    const title = result.length === 0 ? 'Ingen badges? Step it up!'
-                  : result.length > 3 ? result.length + ' badges. isbot er stolt.'
-                  : 'Du har ' + result.length + (result.length === 1 ? ' badge' : ' badges');
-    var badgesMessage = [title].concat(result.map(function(badge){
-      return '`' + badge + '`';
-    })).join('\n');
-    return badgesMessage;
-  } catch (e){
-    console.error(e)
-  }
+module.exports = co.wrap(function*(user, parameter){
+  const result = yield api.myBadges(user);
+  const title = result.length === 0 ? 'Ingen badges? Step it up!'
+                : result.length > 3 ? result.length + ' badges. isbot er stolt.'
+                : 'Du har ' + result.length + (result.length === 1 ? ' badge' : ' badges');
+  var badgesMessage = [title].concat(result.map(function(badge){
+    return '`' + badge + '`';
+  })).join('\n');
+  return badgesMessage;
 });
