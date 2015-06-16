246640b4ccf21951c44dab8f153339c52de140bb
Marius Gundersen <gundersen@gmail.com>
2015-06-16 09:19:06 +0200

    implemented favorites output

--- whatDoILike.js
+++ whatDoILike.js
@@ -4,8 +4,11 @@ const textTable = require('text-table');
 
 module.exports = co.wrap(function* getStatus(user, parameter){
   const result = yield api.favorites(user);
-  
+
   console.log(result);
-  
-  return "Vet du ikke det selv?";
+  if(result.length){
+    return result.map(x => `${x.Name} (${x.Quantity})`).join('\n');
+  }else{
+    return "Du liker ikke is!"
+  }
 });
