53ad512ca1760e6480c1173e3dfd0700782a5f19
Marius Gundersen <me@mariusgundersen.net>
2015-08-25 12:59:49 +0200

    Help should reflect the commands that are possible

--- help.js
+++ help.js
@@ -7,6 +7,9 @@ module.exports = co.wrap(function*(name, user){
     '`isbot jeg er <ansattnummer>` registrer deg så du kan kjøpe is',
     '`isbot kjøp <navn på is>` kjøp en is',
     '`isbot badges` list tilgjengelige badges',
-    '`isbot mine badges` se hvilke badges du har'
+    '`isbot mine badges` se hvilke badges du har',
+    '`isbot hva liker jeg?` se hvilken is du liker best',
+    '`isbot hvordan har du det?` så hyggelig at du spør!',
+    '`isbot det er tomt for <is>` si ifra til sjefen at det er tomt for en is'
   ].join('\n');
 });
