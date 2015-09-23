32c769d64ba8ac80f901aaafe9d04809da0381c2
Marius Gundersen <me@mariusgundersen.net>
2015-09-23 14:28:19 +0200

    third param is slack information

--- registerUser.js
+++ registerUser.js
@@ -1,12 +1,12 @@
 const co = require('co');
 const api = require('./api');
 
-module.exports = co.wrap(function*(slackId, employeeId, slackName){
+module.exports = co.wrap(function*(slackId, employeeId, slack){
   try{
     const result = yield api.register(slackId, employeeId);
     
     if(result.success){
-      return "Aha, jeg veit hvem du er "+slackName+"!";
+      return "Aha, jeg veit hvem du er "+slack.user_name+"!";
     }else{
       throw "oops";
     }
